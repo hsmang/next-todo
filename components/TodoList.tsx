26d2cbd57e70c9602faac9b856b78b4098e84b44
smhan <smhan@simmssoft.com>
2021-04-06 20:32:42 +0900

    todo 숫자

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,23 +1,69 @@
-import React from "react";
+import React, { useMemo, useCallback } from "react";
 import styled from "styled-components";
 import palette from "../styles/palette";
 import { TodoType } from "../types/todo";
 
 
 
+
 const Container = styled.div`
     width: 100%;
 
+    .todo-num {
+        margin-left: 12px;
+    }
+
     .todo-list-header {
         padding: 12px;
+        position: relative;
         border-bottom: 1px solid ${palette.gray};
 
         .todo-list-last-todo {
             font-size: 14px;
+            margin: 0 0 8px;
             span {
-                margin-left: 8px;
+                margin-left: 12px;
             }
         }
+    }
+
+    .todo-list-header-colors {
+        display:flex;
+        .todo-list-header-color-num {
+            display: flex;
+            margin-right: 8px;
+            p {
+                font-size: 14px;
+                line-height: 16px;
+                margin: 0;
+                margin-left: 6px;
+            }
+
+            .todo-list-header-round-color {
+                width: 16px;
+                height: 16px;
+                border-radius: 50%;
+            }
+        }
+    }
+
+    .bg-blue {
+        background-color: ${palette.blue};
+    }
+    .bg-green {
+        background-color: ${palette.green};
+    }
+    .bg-navy {
+        background-color: ${palette.navy};
+    }
+    .bg-orange {
+        background-color: ${palette.orange};
+    }
+    .bg-red{
+        background-color: ${palette.red};
+    }
+    .bg-yellow{
+        background-color: ${palette.yellow};
     }
 `;
 
@@ -27,20 +73,83 @@ interface IProps {
     todos: TodoType[];
 }
 
-const getTodoColorNums = () => {
-    let red = 0;
-    let orange = 0;
-    let yellow = 0;
-    let green = 0;
-    let blue = 0;
-    let navy = 0;
-    todos.forEach((todo) => {
+//* 객체의 문자열 인덱스 사용을 위한 타입
+type ObjectIndexType = {
+    [key: string]: number | undefined;
+};
 
-    })
-}
+
 
 const TodoList: React.FC<IProps> = ({ todos }) => {
 
+
+    //const todoColorNums = useMemo(getTodoColorNums, [todos]);
+
+
+    //* 색깔 객체 구하기 1
+    const todoColorNums = useCallback(() => {
+        let red = 0;
+        let orange = 0;
+        let yellow = 0;
+        let green = 0;
+        let blue = 0;
+        let navy = 0;
+        todos.forEach((todo) => {
+            switch (todo.color) {
+                case "red":
+                    red += 1;
+                    break;
+                case "orange":
+                    orange += 1;
+                    break;
+                case "yellow":
+                    yellow += 1;
+                    break;
+                case "green":
+                    green += 1;
+                    break;
+                case "blue":
+                    blue += 1;
+                    break;
+                case "navy":
+                    navy += 1;
+                    break;
+                default:
+                    break;
+
+            }
+        })
+
+        return {
+            red,
+            orange,
+            yellow,
+            green,
+            blue,
+            navy,
+        };
+    }, [todos]);
+
+    console.log(todoColorNums);
+
+    //* 색깔 객체 구하기 2
+    const todoColorNums2 = useMemo(() => {
+        const colors: ObjectIndexType = {};
+        todos.forEach((todo) => {
+            const value = colors[todo.color];
+            if (!value) {
+                //*존재하지 않던 key라면
+                colors[`${todo.color}`] = 1;
+            } else {
+                //*존재하는 키라면
+                colors[`${todo.color}`] = value + 1;
+            }
+        });
+        return colors;
+
+    }, [todos]);
+
+    console.log(todoColorNums2);
 
     return (
         <Container>
@@ -48,6 +157,14 @@ const TodoList: React.FC<IProps> = ({ todos }) => {
                 <p className="todo-list-last-todo">
                     남은 TODO<span>{todos.length}개</span>
                 </p>
+                <div className="todo-list-header-colors">
+                    {Object.keys(todoColorNums2).map((color, index) => (
+                        <div className="todo-list-header-color-num" key={index}>
+                            <div className={`todo-list-header-round-color bg-${color}`} />
+                            <p>{todoColorNums2[color]}개</p>
+                        </div>
+                    ))}
+                </div>
             </div>
         </Container>
     )
